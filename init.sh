4d8ca3e3209779a56ed76251eae9403a4fca5fdd
Nicholas Guyett <nicholas.guyett@zapways.com>
2017-05-26 17:29:59 -0700

    Include tmux configuration

--- init.sh
+++ init.sh
@@ -1,1 +1,7 @@
+# Delegate to init script in my vim folder
 ./vim/init.sh
+
+# zsh and tmux config only needs to be pointed to their respective dotfile in
+# this repository
+ln -s -b -f $(pwd)/zshrc $HOME/.zshrc
+ln -s -b -f $(pwd)/tmux.conf $HOME/.tmux.conf
