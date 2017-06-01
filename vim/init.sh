6142979ce357264c472687313508ffddf15324a1
Nicholas Guyett <nicholas.guyett@zapways.com>
2017-06-01 11:29:30 -0700

    Make vim init script idempotent and agnostic to the directory of the calling script

--- vim/init.sh
+++ vim/init.sh
@@ -1,2 +1,17 @@
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#!/bin/bash
+
+readonly SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+readonly VUNDLE_REPO="$SCRIPT_DIR/bundle/Vundle.vim"
+
+# symlink this folder to .vim
+if [ ! -d $HOME/.vim ]; then
+  ln -s -b -f $SCRIPT_DIR $HOME/.vim
+fi
+
+# Bootstrap vundle install
+if [ ! -d $VUNDLE_REPO ]; then
+  git clone https://github.com/VundleVim/Vundle.vim.git $VUNDLE_REPO
+fi
+
+# Vundle's install command is idempotent and works for updates as well
 vim +PluginInstall +qall
